eddcc348ef406d7998d1715c633921655d0f1e13
damar wisnu candra prabowo <damar.wisnu1@gmail.com>
2025-05-26 09:02:44 +0700

    penambahan ok

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,5 +23,8 @@ class AppServiceProvider extends ServiceProvider
             if (env('APP_ENV') != 'local') {
             $this->app['request']->server->set('HTTPS', true);
         }
+
+         Schema::defaultStringLength(191);
+         Schema::defaultMorphKeyType('string');
     }
 }
